6d6115d07a87a40177e19d6a07e832a3de954736
Uriel Alonso <uaalonso@apper.ph>
2025-09-30 16:47:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: '.', // explicitly set root folder
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // explicitly set entry point
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],

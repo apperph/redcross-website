7b51747f69ecdd0aa44705f279505dc3788d8cdc
Uriel Alonso <uaalonso@apper.ph>
2025-09-30 17:46:12 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-// If you had a CSS file, you would import it here, e.g.:
-// import './index.css'; 
+import './index.css'; 
 
 // This line finds the root element from index.html and injects the <App /> component.
 ReactDOM.createRoot(document.getElementById('root')).render(

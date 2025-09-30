9ad219df0d40493bb54d209cee8698d8dd6e680b
Uriel Alonso <uaalonso@apper.ph>
2025-09-30 15:05:14 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ this replaces next export
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // ✅ needed when using static export (no Image Optimization API)
-  },
-};
+  output: 'standalone', // ✅ not "export"
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

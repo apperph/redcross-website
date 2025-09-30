722d362b14103f7677447e023de49006a07ef766
Uriel Alonso <uaalonso@apper.ph>
2025-09-30 17:14:09 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './App.jsx';
+// If you had a CSS file, you would import it here, e.g.:
+// import './index.css'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// This line finds the root element from index.html and injects the <App /> component.
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
